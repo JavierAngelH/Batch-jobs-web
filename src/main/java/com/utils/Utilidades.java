b331c1b67f0d38bcaebf26ff20f1466fb7b7943e
Disasterbaby <disasterbaby@gmail.com>
2016-09-28 21:04:18 -0400

    fianl commit

---
+++ src/main/java/com/utils/Utilidades.java
@@ -0,0 +1,12 @@
+package com.utils;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+public class Utilidades {
+
+	public static final String XML_PATH = "http://server.plugstreaming.com:8086/serverinfo";
+
+	 public static final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", new Locale("es","VE"));
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,14 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+<h1>
+	Bienvenido!  
+</h1>
+
+<P>  La hora del sistema es ${serverTime}. </P>
+</body>
+</html>
